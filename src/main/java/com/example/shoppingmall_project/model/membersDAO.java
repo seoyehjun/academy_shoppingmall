e24785508da2a755898e6d50afc1a8533196d953
wonjin <nyg159@gamil.com>
2023-11-30 14:49:30 +0900

    추가

--- src/main/java/com/example/shoppingmall_project/model/membersDAO.java
+++ src/main/java/com/example/shoppingmall_project/model/membersDAO.java
@@ -1,24 +1,14 @@
 package com.example.shoppingmall_project.model;
 
-import java.util.List;
-
 import com.example.shoppingmall_project.model.vo.MembersVO;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
-public interface membersDAO {
-
-	@Select("select * from members")
-	List<MembersVO> selectAll();
-
-	@Insert("INSERT INTO members (members_id, members_password, members_nickname, members_email, members_address, members_detailed_address, members_phone_number, members_name, members_ssn) " + 
-			"VALUES (#{members_id}, #{members_password }, #{members_nickname }, #{members_email }, #{members_address }, #{members_detailed_address }, #{members_phone_number }, #{members_name }, #{members_ssn })")
-	int signUp(MembersVO input);
-
-	
+public interface MembersDAO {
+    @Select("select * from members where members_id = #{members_id} and members_password = #{members_password}")
+    MembersVO selectOne(MembersVO input);
 
 }
