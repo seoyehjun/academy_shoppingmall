c08493f473c940f12dab18ec3a8785593a50c877
wonjin <nyg159@gamil.com>
2023-12-29 13:57:07 +0900

    oket

--- src/main/java/com/example/shoppingmall_project/model/vo/AdminOrdersVO.java
+++ src/main/java/com/example/shoppingmall_project/model/vo/AdminOrdersVO.java
@@ -6,7 +6,7 @@ import java.sql.Date;
 
 @Data
 public class AdminOrdersVO {
-    private int orders_idx, products_price;
+    private int orders_idx, products_price, ordersd_details;
     private String members_id, members_name, products_name, color, size_product, orders_status;
     private Date orders_date;
 
